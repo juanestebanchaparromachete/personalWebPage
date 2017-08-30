d2e5cc59fc991af60fbb3802e4ab250a80663069
juanestebanchaparromachete <je.chaparro10@gmail.com>
2017-08-29 23:02:53 -0500

    End
    Need images

--- main.js
+++ main.js
@@ -45,12 +45,43 @@ window.onload = function () {
         });
     });
 
+    $(function () {
+
+        var $body = $(document);
+        $body.on('click', '.wfPageNextButton, .wfPagePreviousButton', function () {
+            // "Disable" the horizontal scroll.
+            if ($body.scrollTop() !== 0) {
+                $body.scrollTop(0);
+            }
+        });
+
+    });
 
 
-    $('.sendBut').click(function () {
 
-        console.log("do it");
-    })
+    var myform = $("form#myform");
+    myform.submit(function (event) {
+        event.preventDefault();
+
+        // Change to your service ID, or keep using the default service
+        var service_id = "gmail";
+        var template_id = "template_LOXIHisP";
+
+        myform.find("button").text("Sending...");
+        emailjs.sendForm(service_id, template_id, "myform")
+            .then(function () {
+                alert("Sent!");
+                myform.find("button").text("Send");
+            }, function (err) {
+                alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+                myform.find("button").text("Send");
+            });
+        return false;
+    });
+
+
+
+
 
 
 
